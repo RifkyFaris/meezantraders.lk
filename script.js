d45daa856871db162299cc9ee48b6c9a65249bf3
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-01-22 21:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function menuOpen(){
+    document.getElementById('menu').style.display="none"
+    document.getElementById('menuClose').style.display="block"
+    
+    document.getElementById('nav-items').style.display="block"
+    
+}
+function menuClose(){
+    document.getElementById('menu').style.display="block"
+    document.getElementById('menuClose').style.display="none"
+    
+    document.getElementById('nav-items').style.display="none"
+}
